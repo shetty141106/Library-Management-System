1459cdf58f68e6adb5b14cfbb569de906d1ae0ea
CodeSlavve <vsangeeta045@gmail.com>
2026-06-13 20:28:24 +0530

    Created Staff DTO, DAO, Service, Controller

---
+++ lms/src/main/java/com/project/lms/Dao/StaffDao.java
@@ -0,0 +1,94 @@
+package com.project.lms.Dao;
+
+import com.project.lms.Entity.Staff;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class StaffDao {
+
+    Configuration cfg;
+    SessionFactory sf;
+
+    public StaffDao() {
+        cfg = new Configuration();
+        cfg.addAnnotatedClass(Staff.class);
+        sf = cfg.buildSessionFactory();
+    }
+
+    public List<Staff> findAll() {
+
+        Session s = sf.openSession();
+
+        try {
+            return s.createQuery(
+                    "from com.project.lms.Entity.Staff",
+                    Staff.class
+            ).list();
+        }
+        finally {
+            s.close();
+        }
+    }
+
+    public Optional<Staff> findById(int id) {
+
+        Session s = sf.openSession();
+
+        try {
+            return Optional.ofNullable(
+                    s.find(Staff.class, id)
+            );
+        }
+        finally {
+            s.close();
+        }
+    }
+
+    public Staff save(Staff staff) {
+
+        Session s = sf.openSession();
+        Transaction tr = s.beginTransaction();
+
+        try {
+            s.saveOrUpdate(staff);
+            tr.commit();
+            return staff;
+        }
+        catch (Exception e) {
+            tr.rollback();
+            throw e;
+        }
+        finally {
+            s.close();
+        }
+    }
+
+    public void deleteById(int id) {
+
+        Session s = sf.openSession();
+        Transaction tr = s.beginTransaction();
+
+        try {
+
+            Staff staff = s.find(Staff.class, id);
+
+            if (staff != null) {
+                s.remove(staff);
+                tr.commit();
+            }
+            else {
+                tr.rollback();
+            }
+
+        } finally {
+            s.close();
+        }
+    }
+}
