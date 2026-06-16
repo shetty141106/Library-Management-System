91b1f69cc61431587b506149b64e97c197290800
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-06-16 13:07:49 +0530

    ReservationDao

--- lms/src/main/java/com/project/lms/Dao/ReservationDao.java
+++ lms/src/main/java/com/project/lms/Dao/ReservationDao.java
@@ -7,6 +7,7 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -37,8 +38,38 @@ public class ReservationDao {
     }
 
     public void deleteById(Long resid) {
+        Session session=sf.openSession();
+        Transaction tr= session.beginTransaction();
+        try{
+            Reservation reservation=session.find(Reservation.class,resid);
+            session.remove(reservation);
+            tr.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tr.rollback();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            tr.rollback();
+        }
+        finally {
+            session.close();
+        }
+
     }
 
     public Optional<Reservation> findById(Long resid) {
+        Session session=sf.openSession();
+        Reservation reservation=null;
+        try{
+             reservation=session.find(Reservation.class,resid);
+            return Optional.ofNullable(reservation);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+        finally {
+            session.close();
+        }
+
     }
 }
