df523a370bb5ab760930ed9d0e7f7bacc338e2e7
Atharvsurya <atharvsurya007@gmail.com>
2026-06-18 14:45:25 +0530

    Changed application to hibernate

--- lms/src/main/java/com/project/lms/Utility/HibernateUtil.java
+++ lms/src/main/java/com/project/lms/Utility/HibernateUtil.java
@@ -14,7 +14,7 @@ public class HibernateUtil {
 
             // Database configuration credentials defined ONCE
             c.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-            c.setProperty("hibernate.connection.url", "jdbc:mysql://49t2jbFnL9xeerB.root:<PASSWORD>@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/lms-db");
+            c.setProperty("hibernate.connection.url", "jdbc:mysql://gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/lms-db");
             c.setProperty("hibernate.connection.username", "49t2jbFnL9xeerB.root");
             c.setProperty("hibernate.connection.password", "QAA3wiFEbZXk1ZSZ");
             c.setProperty("hibernate.hbm2ddl.auto", "update");
