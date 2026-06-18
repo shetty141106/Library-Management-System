9ac70012efa66c65766dcab2cef716f8111cefb6
Atharvsurya <atharvsurya007@gmail.com>
2026-06-18 14:41:20 +0530

    Cloud password changed

--- lms/src/main/java/com/project/lms/Utility/HibernateUtil.java
+++ lms/src/main/java/com/project/lms/Utility/HibernateUtil.java
@@ -16,7 +16,7 @@ public class HibernateUtil {
             c.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
             c.setProperty("hibernate.connection.url", "jdbc:mysql://49t2jbFnL9xeerB.root:<PASSWORD>@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/lms-db");
             c.setProperty("hibernate.connection.username", "49t2jbFnL9xeerB.root");
-            c.setProperty("hibernate.connection.password", "nq9S5alKASRxDd5B");
+            c.setProperty("hibernate.connection.password", "QAA3wiFEbZXk1ZSZ");
             c.setProperty("hibernate.hbm2ddl.auto", "update");
             c.setProperty("hibernate.show_sql", "true");
             c.setProperty("hibernate.format_sql", "true");
