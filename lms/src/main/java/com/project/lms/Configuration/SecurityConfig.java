b227f812527520ab74a4842f2ca1d9d7ec37857e
Atharvsurya <atharvsurya007@gmail.com>
2026-06-19 17:03:59 +0530

    Render

--- lms/src/main/java/com/project/lms/Configuration/SecurityConfig.java
+++ lms/src/main/java/com/project/lms/Configuration/SecurityConfig.java
@@ -1,17 +1,12 @@
 package com.project.lms.Configuration;
 
-import com.project.lms.Dao.AuthDao;
 import com.project.lms.Security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -30,7 +25,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/index.html").permitAll()
+                        .requestMatchers("/api/auth/**", "/static/**", "/", "/error", "/index.html", "/Frontend/**", "favicon.ico","/assets/**" ).permitAll()
                         // 2. Lock down everything else
                         .anyRequest().authenticated()
                 )
