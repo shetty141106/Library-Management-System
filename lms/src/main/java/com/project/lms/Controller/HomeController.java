0f583488c0df9196226ac9dcdb4f8bfa82e03126
Atharvsurya <atharvsurya007@gmail.com>
2026-06-20 13:39:19 +0530

    Chale to chand tak, nahi toh shaam tak!

---
+++ lms/src/main/java/com/project/lms/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.lms.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Library Management System API is up and running!";
+    }
+}
